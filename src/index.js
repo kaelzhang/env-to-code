180685bcd530d52d1a36e9bd5e28351e32df684c
Kael Zhang <i+github@kael.me>
2019-03-14 22:43:51 +0800

    tests: fixes all

---
+++ src/index.js
@@ -0,0 +1,82 @@
+const stringify = require('js-stringify')
+
+const JUST_RETURN = x => x
+const isArray = (s, delimiter) => delimiter && s.indexOf(delimiter) !== -1
+
+// - no `Function(string)` callback if is not basic string
+const basic = (s, yes, no) => {
+  if (s === undefined || s === '') {
+    return yes(s)
+  }
+
+  if (s === 'true') {
+    return yes(true)
+  }
+
+  if (s === 'false') {
+    return yes(false)
+  }
+
+  const n = Number(s)
+  if (String(n) === s) {
+    return yes(n)
+  }
+
+  return no(s)
+}
+
+const trim = s => s.trim()
+const simpleBasic = s => basic(s, JUST_RETURN, trim)
+
+// - yes `Function(jsonObject)` callback if is json
+// - yes `Function(string)` callback if not json
+const json = (s, testJSON, yes, no) => {
+  if (!testJSON) {
+    return no(s)
+  }
+
+  let parsed
+
+  try {
+    parsed = JSON.parse(s)
+  } catch (error) {
+    return no(s)
+  }
+
+  return yes(parsed)
+}
+
+// No if else callback
+const array = (s, delimiter) => isArray(s, delimiter)
+  ? s.split(delimiter).map(simpleBasic)
+  : s
+
+// convert env variable to JavaScript variable
+exports.js = (s, {
+  arrayDelimiter = ',',
+  testJSON = false
+} = {}) => basic(
+  s,
+  JUST_RETURN,
+  s => json(
+    s,
+    testJSON,
+    JUST_RETURN,
+    s => array(s, arrayDelimiter)
+  )
+)
+
+exports.code = (s, {
+  arrayDelimiter = ',',
+  testJSON = false
+} = {}) => basic(
+  s,
+  stringify,
+  s => json(
+    s,
+    testJSON,
+    // If is an JSON string, just return the string
+    () => s,
+    s => stringify(array(s, arrayDelimiter))
+  )
+)
--- test/env-to-code.js
+++ test/env-to-code.js
@@ -1,7 +1,102 @@
 const test = require('ava')
-const log = require('util').debuglog('env-to-code')
-const env_to_code = require('../src')
+const {
+  code,
+  js
+} = require('../src')
 
-test('description', t => {
-  t.is(true, true)
-})
+const U = undefined
+const CASES = [
+  [
+    '',
+    // arrayDelimiter
+    U,
+    // testJSON
+    U,
+    // code
+    '""',
+    // js variable
+    ''
+  ],
+  ['1', U, U, '1', 1],
+  ['true', U, U, 'true', true],
+  ['false', U, U, 'false', false],
+  ['a,b', U, U, '["a","b"]', ['a', 'b']],
+  ['a,1', U, U, '["a",1]', ['a', 1]],
+  ['a,b', '', U, '"a,b"', 'a,b'],
+  ['{"a":1}', U, true, '{"a":1}', {a: 1}],
+  ['["a", "b"]', U, true, '["a", "b"]', ['a', 'b']],
+  ['["a", "b"]', U, false, '["[\\"a\\"","\\"b\\"]"]', ['["a"', '"b"]']],
+]
+
+const DEFAULT_DELIMITER = ','
+const DEFAULT_TEST_JSON = false
+
+const title = (i, s, options) => {
+  if (!options) {
+    return `${i}: ${s} with no options`
+  }
+
+  const d = 'arrayDelimiter' in options
+    ? JSON.stringify(options.arrayDelimiter)
+    : 'NONE'
+  const j = 'testJSON' in options
+    ? JSON.stringify(options.testJSON)
+    : 'NONE'
+
+  return `${i}: ${s}, ${d}, ${j}`
+}
+
+const runOptions = (i, s, options, codeExpected, jsExpected) => {
+  test(title(i, s, options), t => {
+    t.is(code(s, options), codeExpected, 'code')
+
+    Object(jsExpected) === jsExpected
+      ? t.deepEqual(js(s, options), jsExpected, 'js')
+      : t.is(js(s, options), jsExpected, 'js')
+  })
+}
+
+const runOne = (i, s, arrayDelimiter, testJSON, c, v) => {
+  runOptions(i, s, {
+    arrayDelimiter,
+    testJSON
+  }, c, v)
+
+  if (arrayDelimiter === DEFAULT_DELIMITER && testJSON === DEFAULT_TEST_JSON) {
+    runOptions(i, s, {}, c, v)
+    runOptions(i, s, U, c, v)
+    return
+  }
+
+  if (arrayDelimiter === DEFAULT_DELIMITER) {
+    runOptions(i, s, {
+      testJSON
+    }, c, v)
+    return
+  }
+
+  if (testJSON === DEFAULT_TEST_JSON) {
+    runOptions(i, s, {
+      arrayDelimiter
+    }, c, v)
+  }
+}
+
+const run = ([
+  s, d, j, c, v
+], i) => {
+  const ds = d === U
+    ? [',', U]
+    : [d]
+  const ss = j === U
+    ? [true, false]
+    : [j]
+
+  ds.forEach(dd => {
+    ss.forEach(jj => {
+      runOne(i, s, dd, jj, c, v)
+    })
+  })
+}
+
+CASES.forEach(run)
